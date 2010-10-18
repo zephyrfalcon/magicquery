cbbb0dc28e11c1cf4c7d299ee62362eb78b3e353
Hans Nowak <zephyrfalcon@gmail.com>
2010-10-18 13:38:15 -0400

    refactoring: added CardLoader

--- magicquery.py
+++ magicquery.py
@@ -24,11 +24,6 @@ class MagicSet(object):
             setattr(s, name, xml.find(name).text)
         return s
 
-cp = ConfigParser.ConfigParser()
-cp.read("config.txt")
-xmldir = os.path.expanduser(cp.get('main', 'xmldir'))
-addondir = os.path.expanduser(cp.get('main', 'addondir'))
-
 def extract_data(xml):
     setnode = xml.find('set')
     cards = xml.findall('set/cards/card')
@@ -42,29 +37,43 @@ def get_files_for_sets(sets, dir, suffix):
     files = [os.path.join(dir, fn) for fn in files]
     return files
 
-def load_cards(sets=[]):
-    xmlfiles = get_files_for_sets(sets, xmldir, ".xml")
-    cards = []
-    t1 = time.time()
-    for fn in xmlfiles:
-        print "Loading:", fn
-        x = ET.parse(fn)
-        setnode, cardnodes = extract_data(x)
-        theset = MagicSet.from_xml(setnode)
-        cs = [magiccard.MagicCard.from_xml(cardnode) for cardnode in cardnodes]
-        for c in cs: c._data['set'] = theset
-        cards.extend(cs)
-    t2 = time.time()
-    print "%d cards in %d files, loaded in %.2fs" % (len(cards),
-          len(xmlfiles), t2-t1)
+class CardLoader:
 
-    return cards
+    def __init__(self, sets=None):
+        self.cfg = self._read_configuration()
+        self.sets = sets or []
+        self.cards = []
 
-def load_addons(sets=[]):
-    addon_files = get_files_for_sets(sets, addondir, ".txt")
-    for fn in addon_files:
-        keywords = addons.load_addons(fn)
-        addons.proliferate(cards, keywords) # :-)
+    def _read_configuration(self):
+        cp = ConfigParser.ConfigParser()
+        cp.read("config.txt")
+        return cp
+
+    def load_cards(self):
+        xmldir = os.path.expanduser(self.cfg.get('main', 'xmldir'))
+        xmlfiles = get_files_for_sets(self.sets, xmldir, ".xml")
+        t1 = time.time()
+        for fn in xmlfiles:
+            print "Loading:", fn
+            x = ET.parse(fn)
+            setnode, cardnodes = extract_data(x)
+            theset = MagicSet.from_xml(setnode)
+            cs = [magiccard.MagicCard.from_xml(cardnode) for cardnode in cardnodes]
+            for c in cs: c._data['set'] = theset
+            self.cards.extend(cs)
+        t2 = time.time()
+        print "%d cards in %d files, loaded in %.2fs" % (len(self.cards),
+              len(xmlfiles), t2-t1)
+
+        return self.cards
+
+    def load_addons(self):
+        addondir = os.path.expanduser(self.cfg.get('main', 'addondir'))
+        if addondir:
+            addon_files = get_files_for_sets(self.sets, addondir, ".txt")
+            for fn in addon_files:
+                keywords = addons.load_addons(fn)
+                addons.proliferate(self.cards, keywords) # :-)
 
 def mana_cost_repr(card):
     return "".join(["{%s}" % x for x in card['manacost']])
@@ -78,7 +87,7 @@ class MagicCardDB:
     def query(self, expr):
         self.results = []
         count = 0
-        for card in cards:
+        for card in self.cards:
             count += 1
             if count % 100 == 0: sys.stdout.write("."); sys.stdout.flush()
             try:
@@ -127,7 +136,7 @@ def show_card(card):
     if card['power'] is not null:
         print "%s/%s" % (card['power'], card['toughness'])
 
-def mainloop():
+def mainloop(cards):
     db = MagicCardDB(cards)
 
     while 1:
@@ -148,9 +157,10 @@ if __name__ == "__main__":
 
     opts, args = getopt.getopt(sys.argv[1:], "", [])
 
-    cards = load_cards(sets=args)
-    if addondir:
-        load_addons(sets=args)
-    mainloop()
+    loader = CardLoader(sets=args)
+    loader.load_cards()
+    loader.load_addons()
+
+    mainloop(loader.cards)
 
                 
